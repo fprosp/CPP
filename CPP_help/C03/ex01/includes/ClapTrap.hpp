0c7e2ab511f49e2509dd4c9a5303db13ee400fba
fprosp <francesco.prosperim@hotmail.com>
2023-12-20 14:08:08 +0100

    manca lultim o

--- CPP_help/C03/ex01/includes/ClapTrap.hpp
+++ CPP_help/C03/ex01/includes/ClapTrap.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ClapTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: arecce <arecce@student.42.fr>              +#+  +:+       +#+        */
+/*   By: fprosper <fprosper@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/13 16:14:53 by arecce            #+#    #+#             */
-/*   Updated: 2023/05/16 16:42:22 by arecce           ###   ########.fr       */
+/*   Updated: 2023/12/20 12:46:58 by fprosper         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,8 @@ class ClapTrap
 
 		std::string		Name;
 		unsigned int	Hp;
-		unsigned int	Mp;
 		unsigned int	Power;
+		unsigned int	Mp;
 	
 	public:
 
